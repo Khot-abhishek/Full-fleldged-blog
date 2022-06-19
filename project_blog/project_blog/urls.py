97417dd18a484c72f59f563caa45fb2b96680475
khot-abhishek <abhishekrkhot@gmail.com>
2022-06-19 20:08:16 +0530

    added users app for authentication, added a register form

--- project_blog/project_blog/urls.py
+++ project_blog/project_blog/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+
+    path('register/', views.register, name='user-register'),
 ]
