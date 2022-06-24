75b09112a8b137ff386c3b64f51ba28b6eaa7bd9
khot-abhishek <abhishekrkhot@gmail.com>
2022-06-24 23:17:17 +0530

    added functionality of post creataion fro user and implimented CBV for all functions

--- project_blog/blog/urls.py
+++ project_blog/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from .views import about, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
-    path('about/', views.about, name="blog-about"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('detail/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
+    path('create/', PostCreateView.as_view(), name='blog-create'),
+    path('detail/<int:pk>/update/', PostUpdateView.as_view(), name='blog-update'),
+    path('detail/<int:pk>/delete/', PostDeleteView.as_view(), name='blog-delete'),
+    path('about/', about, name="blog-about"),
 ]
