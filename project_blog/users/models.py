332694dc9f44e579f2ddb158c9c7b45282f55e8d
khot-abhishek <abhishekrkhot@gmail.com>
2022-06-24 19:32:02 +0530

    created user/profile updateform, added functionality for users to update info from there profile page, made user images to appear next to there posts

--- project_blog/users/models.py
+++ project_blog/users/models.py
@@ -2,6 +2,7 @@ from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -10,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} profile"
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (250, 250)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
