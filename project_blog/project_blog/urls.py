d68098e03efcd78bd68c322ad51f805b1bc5823a
khot-abhishek <abhishekrkhot@gmail.com>
2022-06-24 15:30:03 +0530

    made change so that images/static files are being shown in the profile page

--- project_blog/project_blog/urls.py
+++ project_blog/project_blog/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import Settings, settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -17,3 +19,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- project_blog/users/views.py
+++ project_blog/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegiterForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 def register(request):
